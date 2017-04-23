8a693402100db339dd34721ef0865ff7442e6164
Quangthien1994 <nguyenthienquang.it1994@gmail.com>
2017-04-23 13:33:39 +0700

    create mo hinh 3 lop

---
+++ DAO/UserDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+namespace DAO
+{
+    class UserDAO
+    {
+        public List<User> Xem()
+        {
+            List<User> user = new List<User>();
+            return user;
+        }
+    }
+}
---
+++ DTO/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class User
+    {
+        private string id;
+        public string Id { get => id; set => id = value; }
+
+        private string user;
+        public string Account { get => user; set => user = value; }
+
+        public User( string id, string user)
+        {
+            this.user = user;
+            this.id = id;
+        }
+    }
+}
